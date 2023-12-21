3fe148784262e4185f4ab2a7d96892be2b989bb9
Sara Ryfczyńska <walissa13@gmail.com>
2023-12-21 04:34:42 +0100

    Update code

--- window_opener.ino
+++ window_opener.ino
@@ -1,93 +1,154 @@
-#define WINDOW_OPEN_RELAY 10
-#define WINDOW_CLOSE_RELAY 9
-
-#define SWITCH_TEMP_MODE A1
-#define SWITCH_FREQ_MODE A0
-
-#define BUTTON_INCREMENT A2
-#define BUTTON_DECREMENT A3
-
-#define THERMOMETER 12
-
-#define LCD_RS 3
-#define LCD_EN 4
-#define LCD_DB4 5
-#define LCD_DB5 6
-#define LCD_DB6 7
-#define LCD_DB7 8
-
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <LiquidCrystal.h>
 
-OneWire oneWire(THERMOMETER);
-DallasTemperature sensors(&oneWire);
+#define WINDOW_PIN_OPEN 10
+#define WINDOW_PIN_CLOSE 9
 
-LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
+#define THERM_PIN 12
+
+#define LCD_PIN_A 5
+#define LCD_PIN_B 6
+#define LCD_PIN_C 7
+#define LCD_PIN_D 8
+#define LCD_PIN_E 4
+#define LCD_PIN_RS 3
+
+#define SWITCH_PIN_FREQ A0
+#define SWITCH_PIN_TEMP A1
+
+#define BUTTON_PIN_INC A2
+#define BUTTON_PIN_DEC A3
+
+#define OPEN_CLOSE_WINDOW_TIME 5000
+#define BEFORE_BUTTON_TIME 200
+
+OneWire oneWire(THERM_PIN);
+DallasTemperature sensors(&oneWire);
+DeviceAddress thermometer;
+LiquidCrystal lcd(LCD_PIN_RS, LCD_PIN_E, LCD_PIN_A, LCD_PIN_B, LCD_PIN_C, LCD_PIN_D);
+
+int tempBorder = 25;
+int refreshMinutes = 5;
+unsigned long lastCheckMs = 0;
+int lastMode = 0;
 
 void setup() {
+  lcd.begin(16, 2);
   Serial.begin(9600);
+  lcd.clear();
 
   sensors.begin();
 
-  lcd.begin(16,2);
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" sensors.");
+
+  if (!sensors.getAddress(thermometer, 0)) Serial.println("Unable to find thermometer");
+
+  pinMode(SWITCH_PIN_FREQ, INPUT_PULLUP);
+  pinMode(SWITCH_PIN_TEMP, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_INC, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_DEC, INPUT_PULLUP);
+}
+
+void updateWindowStateBasingOnTemp(float temp) {
   lcd.clear();
-
-  pinMode(SWITCH_TEMP_MODE, INPUT_PULLUP);
-  pinMode(SWITCH_FREQ_MODE, INPUT_PULLUP);
-  pinMode(BUTTON_INCREMENT, INPUT_PULLUP);
-  pinMode(BUTTON_DECREMENT, INPUT_PULLUP);
-
+  if (temp > tempBorder) {
+    lcd.print("OTWIERANIE OKNA");
+    windowChange(WINDOW_PIN_OPEN);
+  }
+  if (temp < tempBorder) {
+    lcd.print("ZAMYKANIE OKNA");
+    windowChange(WINDOW_PIN_CLOSE);
+  }
+  lcd.clear();
 }
 
-void window_open() {
-  // pinMode workaround explanation: 
-  // https://electronics.stackexchange.com/a/508672  
-  pinMode(WINDOW_OPEN_RELAY, OUTPUT);
-  Serial.println("HIGH");
-  digitalWrite(WINDOW_OPEN_RELAY, HIGH);
-  delay(5000);
-  Serial.println("LOW");
-  digitalWrite(WINDOW_OPEN_RELAY, LOW);
-  delay(5000);
-  pinMode(WINDOW_OPEN_RELAY, INPUT);
+void windowChange(int windowPin) {
+  pinMode(windowPin, OUTPUT);
+
+  digitalWrite(windowPin, LOW);
+  delay(OPEN_CLOSE_WINDOW_TIME);
+  digitalWrite(windowPin, HIGH);
+
+  pinMode(windowPin, INPUT);
 }
 
-int16_t get_room_temp() {
+float getTemp(DeviceAddress deviceAddress) {
   sensors.requestTemperatures();
-  int16_t temp = sensors.getTempCByIndex(0);
+
+  float temp = sensors.getTempC(deviceAddress);
+
+  if (temp == DEVICE_DISCONNECTED_C) {
+    lcd.print("BLAD ODCZYTU TEMPERATURY");
+    delay(2000);
+    return -1000;
+  }
   return temp;
 }
 
-void print_temp(){  
-  int16_t temp = get_room_temp();
-  Serial.print("T=");
-  Serial.println(temp);
-  lcd.clear();
-  lcd.setCursor(7,0);
-  lcd.print("T=");
-  lcd.print(temp);
+int getMode() {
+  if (digitalRead(SWITCH_PIN_FREQ) == LOW) {
+    return 1;
+  }
+  if (digitalRead(SWITCH_PIN_TEMP) == LOW) {
+    return 3;
+  }
+  return 2;
+
 }
 
-void debug_mode(){
-  print_temp();
-
-  Serial.print("SET_TEMP=");
-  Serial.println(digitalRead(SWITCH_TEMP_MODE));
-  Serial.print("SET_FREQ=");
-  Serial.println(digitalRead(SWITCH_FREQ_MODE));
-
-  Serial.print("BN_I=");
-  Serial.println(digitalRead(BUTTON_INCREMENT));
-  Serial.print("BN_D=");
-  Serial.println(digitalRead(BUTTON_DECREMENT));
-
-  lcd.setCursor(0,1);
- 
+int getIncOrDecValue() {
+  delay(BEFORE_BUTTON_TIME);
+  if (digitalRead(BUTTON_PIN_INC) == LOW) {
+    return 1;
+  }
+  if (digitalRead(BUTTON_PIN_DEC) == LOW) {
+    return -1;
+  }
+  return 0;
 }
 
 void loop() {
-  debug_mode();
+  int mode = getMode();
+  if (lastMode != mode) {
+    lcd.clear();
+  }
+  lastMode = mode;
+  switch (mode) {
+    case 1:
+      lcd.setCursor(0, 0);
+      lcd.print("TRYB TEMP");
+      lcd.setCursor(0, 1);
+      lcd.print(String("OBECNA: ") + tempBorder + " C  ");
+      tempBorder += getIncOrDecValue();
+      if (tempBorder < 0) {
+        tempBorder = 0;
+      };
+      break;
+    case 3:
+      lcd.setCursor(0, 0);
+      lcd.print("TRYB CZAS");
+      lcd.setCursor(0, 1);
+      lcd.print(String("OBECNY: ") + refreshMinutes + " MINUT ");
+      refreshMinutes += getIncOrDecValue();
+      if (refreshMinutes < 1) {
+        refreshMinutes = 1;
+      };
+      break;
+    default:
+      lcd.setCursor(0, 0);
+      lcd.print("TRYB PRACA");
+      float currentTemp = getTemp(thermometer);
+      unsigned long currentTimes = millis();
+      lcd.setCursor(0, 1);
+      lcd.print(String("TEMP: ") + currentTemp + " C  ");
+      if ((currentTimes - lastCheckMs) > refreshMinutes * 60000 || currentTimes < lastCheckMs) {
+        lastCheckMs = currentTimes;
+        updateWindowStateBasingOnTemp(currentTemp);
+      }
+      break;
+  }
 }
 
-// TODO: check if relay remains state after power down
